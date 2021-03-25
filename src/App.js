bb1f63fcd67122bfb9146491bc07822dc679a621
Edgar Pena <edgarignaciopena@gmail,com>
2021-03-25 14:06:34 -0700

    working on button functinality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Table from "react-bootstrap/Table";
 import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
 //state is kind of like a global variable, when it updates it will
 //rerender the component you are in. When it changes it will show you
@@ -35,48 +38,66 @@ function App() {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         //the users becomes what the data.results is
         setUsers(data.results);
       });
   }
-  //Sort ex. users.sort((a,b) => a.dob.age - b.dob.age)
 
-  console.log(users);
+  const sortUsers = function sortUsers() {
+    users.sort((a, b) => a.dob.age - b.dob.age);
+    console.log(users);
+  };
 
   return (
     <div>
-      <Table striped bordered hover>
-        <thead></thead>
-        <tbody>
-          {/* Table row for first and last */}
-          <div>
-            {users.map((user) => (
-              <tr>
-                {/* image of person */}
-                <td>
-                  <Image src={user.picture.large} rounded />
-                </td>
-                {/* persons first and last name */}
-                <td>
-                  {user.name.first} {user.name.last}
-                </td>
-                {/* users cell phone number */}
-                <td>
-                  Phone Number:{` `}
-                  {user.cell}
-                </td>
-                {/* users location */}
-                <td>
-                  {user.location.city},{` `}
-                  {user.location.state},{` `}
-                  {user.location.country}
-                </td>
-              </tr>
-            ))}
-          </div>
-        </tbody>
-      </Table>
+      <Container className="title">
+        <h1>Employee Directory</h1>
+      </Container>
+      <Container className="info">
+        <Row>
+          <Button type="button" onClick={sortUsers}>
+            Sort by Age
+          </Button>
+        </Row>
+        <Row>
+          <Table className="table" striped bordered hover>
+            <thead></thead>
+            <tbody>
+              {/* Table row for first and last */}
+
+              {users.map((user) => (
+                <tr className="tabelRow">
+                  {/* image of person */}
+                  <td className="td">
+                    <Image src={user.picture.large} rounded />
+                  </td>
+                  {/* persons first and last name */}
+                  <td className="td">
+                    {user.name.first}
+                    {` `}
+                    {user.name.last}
+                  </td>
+                  <td className="td">
+                    Age:{` `}
+                    {user.dob.age}
+                  </td>
+                  {/* users cell phone number */}
+                  <td className="td">
+                    Phone Number:{` `}
+                    {user.cell}
+                  </td>
+                  {/* users location */}
+                  <td className="td">
+                    {user.location.city},{` `}
+                    {user.location.state},{` `}
+                    {user.location.country}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Row>
+      </Container>
     </div>
   );
 }
